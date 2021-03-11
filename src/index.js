f152fd2e99632250f6239b797e8cb20642fb852c
ErikaMaer <46538589+ErikaMaer@users.noreply.github.com>
2021-03-11 21:44:26 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if (str.length % 2 !== 0) return false;
+    let arrayBrackets = bracketsConfig.map(a => a[0] + a[1]);
+    let strLen = 0;
+    while (str.length !== strLen) {
+        strLen = str.length;
+        arrayBrackets.forEach(brackets => {
+            str = str.replace(brackets, '');
+        });
+    }
+    return str.length === 0;
 }
+
